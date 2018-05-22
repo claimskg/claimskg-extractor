c06c5eca1a74833304f1bf4f3829365d1478a4b0
Eduardo <eduardogcortes8@gmail.com>
2018-05-22 17:39:04 -0300

    Publica - for each page, it get the list of fact links

--- ce/publica.py
+++ ce/publica.py
@@ -12,9 +12,26 @@ def get_all_claims(criteria):
     page = request.read()
     soup = BeautifulSoup(page, 'lxml')
 
+    # Get the number of pages
     pages_links = soup.findAll('a', {"class": "page-link"})
     number_of_pages = int(pages_links[::-1][1].text)
     print('Number of pages: ' + str(number_of_pages))
+
+    # For each page
+    for page_i in range(number_of_pages):
+        page_i += 1
+        print('Page ' + str(page_i) + '|' + str(number_of_pages))
+        url = 'https://apublica.org/checagem/page/' + str(page_i) + '/'
+        print(url)
+        user_agent = 'Mozilla/5.0'
+        request = urllib2.urlopen(urllib2.Request(url, data=None, headers={'User-Agent': user_agent}))
+        page = request.read()
+        soup = BeautifulSoup(page, 'lxml')
+        fact_links = [fl.a['href'] for fl in soup.findAll('div', {"class": "card"})]
+        for fact in fact_links:
+            print(fact)
+
+
 
     pdf = pd.DataFrame(claims)
     return pdf
