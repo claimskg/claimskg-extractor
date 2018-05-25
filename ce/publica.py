2b4cbde192d605d0230617e856dcd47320fab933
eduardogc8 <eduardogcortes8@gmail.com>
2018-05-25 10:25:12 -0300

    Publica - get title

--- ce/publica.py
+++ ce/publica.py
@@ -35,16 +35,15 @@ def get_all_claims(criteria):
                 break
             print(f_link)
             soup2 = get_soup(f_link)
-
+            title_ = soup2.find('title').text
             # claims_ = [claim.text for claim in soup2.find_all('strong') if u'\u201c' in claim.text]
             # credibilities_ = [l['alt'] for l in soup2.find_all('img', {'class': 'alignleft'}) if '-400.jpg' in l['src']]
             # url_ = f_link
 
             contr = 0
-            claims_ = []
             refered_links = []
             date_ = soup2.find('span', {'class': 'date'}).text
-            claim_ = new_claim(f_link, date_)
+            claim_ = new_claim(f_link, date_, title_)
             stop = False
             for c in soup2.find('div', {'class', 'post-contents'}).contents:
                 if c.name is None: continue
@@ -52,7 +51,7 @@ def get_all_claims(criteria):
                     if stop:
                         claim_.setRefered_links(refered_links)
                         claims.append(claim_.getDict())
-                        claim_ = new_claim(f_link, date_)
+                        claim_ = new_claim(f_link, date_, title_)
                         stop = False
                     contr = 1
                     continue
@@ -80,7 +79,7 @@ def get_all_claims(criteria):
             if stop:
                 claim_.setRefered_links(refered_links)
                 claims.append(claim_.getDict())
-
+    print('Number of claims: '+str(len(claims)))
     pdf = pd.DataFrame(claims)
     return pdf
 
@@ -92,9 +91,10 @@ def get_soup(url):
     return BeautifulSoup(page, 'lxml')
 
 
-def new_claim(f_link, date):
+def new_claim(f_link, date, title):
     claim_ = claim_obj.Claim()
     claim_.setUrl(f_link)
+    claim_.setTitle(title)
     date_ = date.strip().split()
     date_ = "-".join([date_[4], date_[2], date_[0]])
     claim_.setDate(dateparser.parse(date_).strftime("%Y-%m-%d"))
